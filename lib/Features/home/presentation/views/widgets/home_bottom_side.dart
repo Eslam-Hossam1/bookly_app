ab928c4098c868b87ac80a9a26e657d87b39305c
Eslam_Hossam1 <eslamhossam.tech@gmal.com>
2024-08-22 22:10:34 +0300

    add google font , create best seller item part 1

--- lib/Features/home/presentation/views/widgets/home_bottom_side.dart
+++ lib/Features/home/presentation/views/widgets/home_bottom_side.dart
@@ -1,16 +1,18 @@
+import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeBottomSide extends StatelessWidget {
   const HomeBottomSide({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.only(left: 24, right: 24, top: 48),
+    return Padding(
+      padding: EdgeInsets.only(left: 24, right: 24, top: 48, bottom: 24),
       child: Column(
         children: [
-          Row(
+          const Row(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               Text(
@@ -19,6 +21,23 @@ class HomeBottomSide extends StatelessWidget {
               ),
             ],
           ),
+          SizedBox(
+            height: 105,
+            child: Row(
+              children: [
+                AspectRatio(
+                  aspectRatio: 70 / 110,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        image: const DecorationImage(
+                            fit: BoxFit.cover,
+                            image: AssetImage(AssetsData.testImage))),
+                  ),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
