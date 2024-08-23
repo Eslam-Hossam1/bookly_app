9fd8f53a2b7d096043cf792fede87f9ecd3356e6
Eslam_Hossam1 <eslamhossam.tech@gmal.com>
2024-08-23 05:10:54 +0300

    create best seller sliver list and delete home bottom widget

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,7 +1,9 @@
+import 'package:bookly_app/Features/home/presentation/views/widgets/best_seller_item.dart';
+import 'package:bookly_app/Features/home/presentation/views/widgets/best_seller_sliver_list.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/featured_list_view.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/featured_list_view_item.dart';
-import 'package:bookly_app/Features/home/presentation/views/widgets/home_bottom_side.dart';
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -9,8 +11,41 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [CustomAppBar(), FeaturedListView(), HomeBottomSide()],
+    return const CustomScrollView(
+      slivers: [
+        const SliverToBoxAdapter(child: CustomAppBar()),
+        const SliverToBoxAdapter(child: FeaturedListView()),
+        const SliverPadding(
+          padding: EdgeInsets.only(
+            left: 30,
+            right: 30,
+            top: 51,
+          ),
+          sliver: SliverToBoxAdapter(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  "Best Seller",
+                  style: Styles.textStyle18,
+                ),
+              ],
+            ),
+          ),
+        ),
+        const SliverToBoxAdapter(
+          child: SizedBox(
+            height: 20,
+          ),
+        ),
+        SliverPadding(
+          padding: const EdgeInsets.only(
+            left: 30,
+            right: 30,
+          ),
+          sliver: BestSellerSliverList(),
+        )
+      ],
     );
   }
 }
